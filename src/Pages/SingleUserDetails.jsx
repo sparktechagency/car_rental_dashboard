4581b3edcc8bc6a110aa9dfa0a668bd7c397996a
shukumar-542 <shukumar542@gmail.com>
2024-08-04 17:42:53 +0600

    Enhancement single user details page

--- src/Pages/SingleUserDetails.jsx
+++ src/Pages/SingleUserDetails.jsx
@@ -29,127 +29,128 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { IoArrowBackSharp } from "react-icons/io5";
-import { MdPadding } from "react-icons/md";
+import { MdOutlineLibraryAddCheck, MdPadding } from "react-icons/md";
+import { IoMdClose } from "react-icons/io";
 
 const SingleUserDetails = () => {
   return (
 
     // <div className="container mx-auto p-5">
-    <div className="bg-white p-6 rounded-lg shadow-md">
+    <div className="bg-white  rounded-lg shadow-md">
       <div className="flex justify-between items-center px-3 my-2 pt-5">
-        <div className="flex items-center gap-2 mb-3 p-5">
+        <div className="flex items-center gap-2 mb-3">
           <Link
             to={-1}
-            className="bg-blue-500 py-1 px-2 rounded-md flex items-center gap-1 text-white"
+            className=" py-1 rounded-md flex items-center gap-1 "
           >
-            <IoArrowBackSharp /> back
+            <IoArrowBackSharp />
           </Link>
-          <p className="text-xl">New Membership Request</p>
+          <p className="text-xl">Membership Application of Faysal Rahman</p>
         </div>
       </div>
-      <div className="grid grid-cols-2 gap-4">
+      <div className="grid grid-cols-2 gap-4 p-6">
         <div className="grid grid-cols-2">
           <div>
-            <p>
-              <strong>Name</strong>
+            <p >
+              Name
             </p>
             <p>
-              <strong>Date of Birth</strong>
+              Date of Birth
             </p>
             <p>
-              <strong>Place of Birth</strong>n
+              Place of Birth
             </p>
             <p>
-              <strong>License No</strong>
+              License No
             </p>
             <p>
-              <strong>Passport</strong>
+              Passport
             </p>
             <p>
-              <strong>Email</strong>
+              Email
             </p>
             <p>
-              <strong>Phone Number</strong>
+              Phone Number
             </p>
             <p>
-              <strong>Profession</strong>
+              Profession
             </p>
             <p>
-              <strong>What's your religion?</strong>
+              What's your religion?
             </p>
             <p>
-              <strong>Do you have children?</strong>
+              Do you have children?
             </p>
             <p>
-              <strong>Do you have pets?</strong>
+              Do you have pets?
             </p>
             <p>
-              <strong>Do you own a vehicle?</strong>
+              Do you own a vehicle?
             </p>
             <p>
-              <strong>Are you willing to swap your vehicles?</strong>
+              Are you willing to swap your vehicles?
             </p>
             <p>
-              <strong>Are you owner or leasing your property?</strong>
+              Are you owner or leasing your property?
             </p>
             <p>
-              <strong>
-                Will you be able to provide approval from owner for temp swap?
-              </strong>
+
+              Will you be able to provide approval from owner for temp swap?
+
             </p>
             <p>
-              <strong>Is your property insured?</strong>
+              Is your property insured?
             </p>
           </div>
 
           <div>
             <p>
-              <strong>:</strong> Foysal Rahman
+              : Foysal Rahman
             </p>
             <p>
-              <strong>:</strong> 24/04/2002
+              :  24/04/2002
             </p>
             <p>
-              <strong>:</strong> Tokyo, Japan
+              : Tokyo, Japan
             </p>
             <p>
-              <strong>:</strong> 34546547675
+              : 34546547675
             </p>
             <p>
-              <strong>:</strong> 658567899769
+              : 658567899769
             </p>
             <p>
-              <strong>:</strong> williejenning@gmail.com
+              : williejenning@gmail.com
             </p>
             <p>
-              <strong>:</strong> (702) 555-0122
+              : (702) 555-0122
             </p>
             <p>
-              <strong>:</strong> Businessman
+              : Businessman
             </p>
             <p>
-              <strong>:</strong> Islam
+              : Islam
             </p>
             <p>
-              <strong>:</strong> Yes
+              : Yes
             </p>
             <p>
-              <strong>:</strong> Yes
+              : Yes
             </p>
             <p>
-              <strong>:</strong> Yes
+              : Yes
             </p>
             <p>
-              <strong>:</strong> Yes
+              : Yes
             </p>
             <p>
-              <strong>:</strong> Yes
+              :Yes
             </p>
             <p>
-              <strong>:</strong> Yes
+              : Yes
             </p>
             <p>
-              <strong>:</strong> Yes
+              : Yes
             </p>
           </div>
         </div>
@@ -158,112 +159,110 @@ const SingleUserDetails = () => {
         <div className="grid grid-cols-2">
           <div>
             <p>
-              <strong>Will your utilities be up to date for swap?</strong>
+              Will your utilities be up to date for swap?
             </p>
             <p>
-              <strong>What do you want to swap and for how long?</strong>
+              What do you want to swap and for how long?
             </p>
             <p>
-              <strong>
-                Do you want to arrive on departure or depart on arrival?
-              </strong>
+              Do you want to arrive on departure or depart on arrival?
             </p>
             <p>
-              <strong>Dates of travel?</strong>
+              Dates of travel?
             </p>
             <p>
-              <strong>Travel Start</strong>
+              Travel Start
             </p>
             <p>
-              <strong>Destination</strong>
+              Destination
             </p>
             <p>
-              <strong>State</strong>
+              State
             </p>
             <p>
-              <strong>County</strong>
+              County
             </p>
             <p>
-              <strong>Country</strong>
+              Country
             </p>
             <p>
-              <strong>Travel End</strong>
+              Travel End
             </p>
             <p>
-              <strong>Destination</strong>
+              Destination
             </p>
             <p>
-              <strong>State</strong>
+              State
             </p>
             <p>
-              <strong>County</strong>
+              Country
             </p>
             <p>
-              <strong>Country</strong>
+              Country
             </p>
             <p>
-              <strong>Purpose of travel?</strong>
+              Purpose of travel?
             </p>
           </div>
 
           <div>
             <p>
-              <strong>:</strong> Yes
+              : Yes
             </p>
             <p>
-              <strong>:</strong> I want to swap for anything from low to high
+              : want to swap for anything from low to high
               value products and want to swap for a long time, at least 2-5
               years.
             </p>
             <p>
-              <strong>:</strong> Arrive on departure
+              : Arrive on departure
             </p>
             <p>
-              <strong>:</strong>
+              :
             </p>
             <p>
-              <strong>:</strong>
+              :
             </p>
             <p>
-              <strong>:</strong> New York
+              New York
             </p>
             <p>
-              <strong>:</strong> St. Celina
+              : St. Celina
             </p>
             <p>
-              <strong>:</strong> Rd. Santa Ana
+              : Rd. Santa Ana
             </p>
             <p>
-              <strong>:</strong> USA
+              : USA
             </p>
             <p>
               <strong></strong>
             </p>
             <p>
-              <strong>:</strong> 3891 Ranchview
+              : 3891 Ranchview
             </p>
             <p>
-              <strong>:</strong> California
+              : California
             </p>
             <p>
-              <strong>:</strong> Delaware
+              : Delaware
             </p>
             <p>
-              <strong>:</strong> USA
+              : USA
             </p>
             <p>
-              <strong>:</strong> Business
+              : Business
             </p>
           </div>
         </div>
       </div>
-      <div className="flex justify-center gap-4 mt-4">
-        <button className="bg-blue-500 text-white py-2 px-4 rounded">
-          Approve
+      <div className="flex justify-center gap-4 mt-4 pb-5">
+        <button className="bg-blue-500 text-white py-2 px-4 rounded flex items-center gap-1 text-sm">
+        <MdOutlineLibraryAddCheck /><span>Approved</span>
         </button>
-        <button className="bg-red-500 text-white py-2 px-4 rounded">
-          Decline
-        </button>
+        <button className="bg-red-500 text-white py-2 px-4 rounded flex items-center text-sm">
+          <IoMdClose /> <span>Decline</span>        
+          </button>
       </div>
     </div>
   );
