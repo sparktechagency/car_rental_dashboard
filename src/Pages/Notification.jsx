ce0545e976d2d7f269e1b82b2e34bd5303ce6692
shukumar-542 <shukumar542@gmail.com>
2024-08-04 18:00:38 +0600

    Enhancement notification page

--- src/Pages/Notification.jsx
+++ src/Pages/Notification.jsx
@@ -1,97 +1,71 @@
-import { Modal } from 'antd';
-import React, { useState } from 'react'
+import { Button, Table } from 'antd';
+import { IoArrowBackSharp } from 'react-icons/io5';
+import { RiDeleteBin6Line } from 'react-icons/ri';
+
+
 
 const data = [
     {
-        key: "1",
-        title: "Tushar ",
-        email: "tushar@gmail.com",
-        date: "18 Jul, 2023  4:30pm",
-        item: "Banasree",
-        status: false,
-        price: "500",
-        balance: "600",
-        image: "https://img.freepik.com/free-photo/everything-is-okay-cheerful-friendly-looking-caucasian-guy-with-moustache-beard-raising-hand-with-ok-great-gesture-giving-approval-like-having-situation-control_176420-22386.jpg"
+        key: '1',
+        notification: 'A new user has applied for gold membership packages and waiting for approval, review the application for approval.',
+        time: 'Just Now',
+    },
+    {
+        key: '2',
+        notification: 'Swap ID #12344 failed due to insufficient balance on User A\'s account. Swap ID #12344 failed due to insufficient balance on User A\'s account.',
+        time: '30 min ago',
+    },
+    {
+        key: '3',
+        notification: 'Swap ID #12345 between User A and User B has been successfully completed. Swap ID #12345 between User A and User B has been successfully completed.',
+        time: '6 hours ago',
     },
 ];
 const Notification = () => {
-    const [page, setPage] = useState( new URLSearchParams(window.location.search).get('page') || 1);
-    const handlePageChange=(page)=>{
-        setPage(page);
-        const params = new URLSearchParams(window.location.search);
-        params.set('page', page);
-        window.history.pushState(null, "", `?${params.toString()}`);
-    }
-    const [openAddModel, setOpenAddModel] = useState(false);
-    const [currentNotification, setCurrentNotification] = useState({})
-    const handleReadAll = () => {
 
-    }
-    const handleRead = (id) => {
 
+    const columns = [
+        {
+            dataIndex: 'notification',
+            key: 'notification',
+            render: text => <span>{text}</span>,
+        },
+        {
+            dataIndex: 'time',
+            key: 'time',
+            width: '150px',
+            render: text => <span>{text}</span>,
+        },
+        {
+            key: 'action',
+            width: '50px',
+            render: (text, record) => (
+                <Button type="text" icon={<RiDeleteBin6Line size={20} className='text-[#D9000A]' />} onClick={() => handleDelete(record.key)} />
+            ),
+        },
+    ];
+    const handleDelete = key => {
+        console.log(`Delete notification with key: ${key}`);
     }
     return (
         <div>
             <div className="flex justify-between items-center gap-4">
-                <h3 className="text-[#242424] text-2xl font-bold">Notifications</h3>
-                <button onClick={handleReadAll} className="text-[#F27405] font-semibold border-2 border-[#F27405] px-6 py-2">
-                    Read all
-                </button>
-            </div>
-            <div className="flex justify-start items-start flex-col gap-2 py-8 px-3">
-                {data.map((item) => (
-                    <div
-                        className={`flex justify-between items-center w-full ${item?.status ? "bg-[#FBFBFB]" : " bg-[#F8F1E6]"
-                        } p-3 py-5 rounded-lg`}
-                        key={item?.key}
-                    >
-                        <div>
-                            <div className="flex justify-start items-center gap-8 mb-1 text-[#919191]">
-                                <h3 className="text-[#555555] font-bold">{item?.title}</h3>
-                                <p>{item?.date}</p>
-                            </div>
-                            <div className="flex justify-start items-center gap-2 text-[#919191]">
-                                <h3>{item?.item}</h3>
-                                <p>{item?.price}</p>
-                            </div>
-                        </div>
-                        <button
-                            onClick={() => {
-                                setOpenAddModel(true);
-                                handleRead(item?.id)
-                                setCurrentNotification(item)
-                            }}
-                            className="text-[#F27405] font-medium text-lg"
-                        >
+                <h3 className="text-[#242424] text-[20px] font-semibold flex items-center gap-2"> <IoArrowBackSharp />Notifications</h3>
 
-                            View
-                        </button>
-                    </div>
-                ))}
             </div>
-            <Modal
-                centered
-                open={openAddModel}
-                onCancel={() => setOpenAddModel(false)}
-                width={600}
-                footer={false}
-            >
-                <div className="p-5 min-h-96 relative">
-                    <div>
-                        <div className="flex justify-start items-center gap-8 mb-1 text-[#919191]">
-                            <h3 className="text-[#555555] font-bold">{currentNotification?.title}</h3>
-                            <p>{currentNotification?.date}</p>
-                        </div>
-                        <div className="flex justify-start items-center gap-2 text-[#919191] mt-3">
-                            <h3>{currentNotification?.item}</h3>
-                            <p>{currentNotification?.price}</p>
-                        </div>
-                    </div>
-                    {/* <button className="bg-[#F27405] p-3 px-6 text-[#FEFEFE] absolute bottom-6 right-6">
-            Visit page
-          </button> */}
-                </div>
-            </Modal>
+            <div>
+                <h2 className='text-[18px] font-semibold py-2'>Total 128 Notifications</h2>
+                <Table columns={columns} dataSource={data} pagination={{
+                    pageSize: 5,
+                    showTotal: (total, range) => `Showing ${range[0]}-${range[1]} out of ${total}`,
+                    locale: {
+                        items_per_page: '',
+                        prev_page: 'Previous',
+                        next_page: 'Next',
+                    },
+                }}
+                className="custom-pagination"   />
+            </div>
         </div>
     );
 }
