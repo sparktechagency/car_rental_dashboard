46aa79a3d64596207b82df65d2ae751ecefc6e89
foisaluddin400 <foisalrk2@gmail.com>
2025-01-04 12:56:52 +0600

    update

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-import { RouterProvider } from 'react-router-dom'
-import { router } from './src/Routes/Routes.jsx'
+
+
 import { Provider } from 'react-redux'
-import { store } from './src/redux/store.jsx'
+import { store } from './redux/store'
 import { Toaster } from 'sonner'
+import { RouterProvider } from 'react-router-dom'
+import { router } from './Routes/Routes'
+// import { router } from './Routes/Routes.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
